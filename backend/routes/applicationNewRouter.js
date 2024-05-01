2a282b3e0f6cb7aa7a8d87d3ee8439345d5cff54
shruuti321 <goelshruti2003@gmail.com>
2024-05-02 00:01:47 +0530

    withdraw button

--- backend/routes/applicationNewRouter.js
+++ backend/routes/applicationNewRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { approveApplication, createApplication, forwardApplication, getAllApplicationByTag, getAllApplications , getMyApplications, getApplicationsByCommenterId, rejectApplication, getCommentByApplication} from "../controllers/applicationNewController.js";
+import { approveApplication, createApplication, forwardApplication, getAllApplicationByTag, getAllApplications , getMyApplications, getApplicationsByCommenterId, rejectApplication, withDrawApplication,getCommentByApplication} from "../controllers/applicationNewController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
@@ -9,6 +9,7 @@ router.get("/getall",isAuthenticated,getAllApplications)
 router.get("/bycommenter",isAuthenticated,getApplicationsByCommenterId);
 router.put("/:applicationId/:commentId/approve",isAuthenticated, approveApplication);
 router.put("/:applicationId/:commentId/reject", isAuthenticated, rejectApplication);
+router.put("/:applicationId/withdraw", isAuthenticated, withDrawApplication);
 router.put('/:applicationId/forward', isAuthenticated, forwardApplication);
 router.get("/getalltag",isAuthenticated,getAllApplicationByTag)
 router.get("/getMyApplications",isAuthenticated,getMyApplications)
